9fc3a151c28c16d41bb6732b73f49006a918f8cd
Lone Wolf <nbtafelberg@gmail.com>
2023-11-28 06:36:33 +0000

    Update syscheck.php

--- syscheck.php
+++ syscheck.php
@@ -1,4 +1,9 @@
 <?php
+/***
+    Simple stand alone Email Script when disk space hits over 80%
+    Paul Clevett / Lone Wolf 2023
+    https://www.wolf-grid.com
+*/
 function df($itemdata) {
     return round($itemdata / 1024 / 1024 / 1024, 2) . "gb";
 }
@@ -10,5 +15,5 @@ echo df($freespace) ." ".df($totalspace) . " " . $percent . "%" . PHP_EOL;
 
 if ($percent>80) {
     $message="Server " . getHostName() . " is running out of disk space";
-    shell_exec('echo "' . $message . '" | mail -s "***DISK SPACE WARNING***" sales@garagemasteronline.com');
+    shell_exec('echo "' . $message . '" | mail -s "***DISK SPACE WARNING***" YOUR-EMAIL-ADDRESS-HERE');
 }
